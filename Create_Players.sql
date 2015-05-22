bcf744371e076f1c1e65b3297dc8aea7f60f2c66
Thiago Cardoso <intelxd@gmail.com>
2015-05-21 22:51:44 -0300

    Início da informação
    Somente peguei como fazer um multiple insert, porque tinha que ter como fazer!!!

---
+++ Create_Players.sql
@@ -0,0 +1,9 @@
+
+/* Modelo de Multi-Insert no ORACLE
+ * Fonte: http://www.techonthenet.com/oracle/questions/insert_rows.php
+INSERT ALL
+  INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
+  INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
+  INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
+SELECT * FROM dual;
+*/
