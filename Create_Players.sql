d44fe2c8a2240d3cc97df55271cd16005843cc57
Thiago Cardoso <intelxd@gmail.com>
2015-05-26 20:36:16 -0300

    Novo modo de insert
    Explicado que não se faz multiple insert com valores definidos

--- Create_Players.sql
+++ Create_Players.sql
@@ -6,4 +6,10 @@ INSERT ALL
   INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
   INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
 SELECT * FROM dual;
+
+Só dá certo quando vem atrelado a colunas vindas de um select
+*/
+
+/* Modelo de Insert no ORACLE
+INSERT INTO minhatabela (coluna1, ... colunan) VALUES ('valor1', ... 'valorn');
 */
